05152ece0ec0863441f790cc81278744e4e8e3e8
Sampann2007 <75363328+Sampann2007@users.noreply.github.com>
2021-05-09 16:51:10 +0530

    Add files via upload

---
+++ MyQuizGame/MatterJSBoilerPlate-master/Quiz.js
@@ -0,0 +1,51 @@
+class Quiz{
+    getState(){
+        var gameStateRef=database.ref('gameState');
+        gameStateRef.on("value",function(data){
+            gameStateRef=data.val();
+        })
+    }
+    update(state){
+        database.ref('/').update({
+            gameState:state
+        })
+    }
+    async start(){
+        if(gameState===0){
+            contestant=new Contestant();
+            var contestantCountRef=await database.ref('contestantCount').once('value');
+            if(contestantCountRef.exists()){
+                contestantCount=contestantCountRef.val();
+                contestant.getCount();
+            }
+            question=new Question();
+            question.display();
+        }
+    }
+    play(){
+        question.hide();
+        background("yellow");
+        fill(0);
+        textSize(30);
+        text("Result Of The Quiz",340,50);
+        text("----------------------",320,65)
+        Contestant.getPlayerInfo();
+        if(allContestant!==undefined){
+            var display_answer=230;
+            fill("blue");
+            textSize(20);
+            text("NOTE: Contestant who answered correct are highlighted in green color!",130,230);
+            for(var plr in allContestant){
+                var correctAns=1;
+                if(correctAns===allContestant[plr].answer)
+                fill("green");
+                else
+                    fill("red");
+
+                    display_answer+=30;
+                    textSize(30);
+                    text(allContestant[plr].name+":"+allContestant[plr].answer,250,display_answer)
+            }
+        }
+    }
+}
---
+++ MyQuizGame/MatterJSBoilerPlate-master/sketch.js
@@ -0,0 +1,41 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var gameState=0;
+var contestantCount;
+var database;
+var quiz;
+var question;
+var contestant;
+
+
+
+function setup() {
+canvas=	createCanvas(850,400);
+database=firebase.database();
+quiz= new Quiz();
+quiz.getState();
+quiz.start();
+
+	engine = Engine.create();
+	world = engine.world;
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  background("pink");
+  if(contestantCount===4){
+	  quiz.update(1);
+  }
+  if(gameState===1){
+	  clear();
+	  quiz.play();
+  }
+}
+
+
+
